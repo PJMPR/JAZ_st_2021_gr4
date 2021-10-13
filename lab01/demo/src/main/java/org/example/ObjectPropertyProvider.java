4763908ccc116b63b15d88fbd046672db169c050
NataliaSkonieczna <natalia.skonieczna2001@wp.pl>
2021-10-13 18:35:39 +0200

    lab01
    Metoda wybierania getterów i setterów

--- lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -10,6 +10,12 @@ public class ObjectPropertyProvider {
 
     public List<Method> getPublicGetters(Class<?> clazz){
 
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods){
+            if(method.getName().startsWith("get")&&method.getName().startsWith("set") && !void.class.equals(method.getReturnType()) && (method.getParameterTypes().length == 0) ){
+                System.out.println("getter: " + method);
+            }
+        }
         return Arrays.stream(clazz.getDeclaredMethods()).toList();
 
     }
@@ -17,11 +23,26 @@ public class ObjectPropertyProvider {
 
     public List<Method> getPublicSetters(Class<?> clazz){
 
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods){
+            if(method.getName().startsWith("set") && method.getParameterTypes().length != 1 && void.class.equals(method.getReturnType()) && method.getParameterTypes().length == 1)
+                System.out.println("setter: " + method);
+        }
+
         return Arrays.stream(clazz.getDeclaredMethods()).toList();
     }
 
 
     public List<Field> getFieldsForPublicProperties(Class<?> clazz){
+
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods){
+            if(method.getName().startsWith("get")&&method.getName().startsWith("set") && !void.class.equals(method.getReturnType()) && (method.getParameterTypes().length == 0) ){
+                System.out.println("getter: " + method);
+            }
+            if(method.getName().startsWith("set") && method.getParameterTypes().length != 1 && void.class.equals(method.getReturnType()) && method.getParameterTypes().length == 1)
+                System.out.println("setter: " + method);
+        }
 
         return Arrays.stream(clazz.getDeclaredFields()).toList();
 
