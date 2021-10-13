2cbf2ac75fd33ffe02159507be49a6de6d443c33
Ghostre9000 <77153195+Ghostre9000@users.noreply.github.com>
2021-10-13 17:22:44 +0200

    Uplouding the basic files

---
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -0,0 +1,36 @@
+package org.example;
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+public class ObjectPropertyProvider {
+
+    public List<Method> getPublicGetters(Class<?> clazz){
+
+        Method[] methods = SampleClass.class.getMethods();
+        for (Method method : methods) {
+            System.out.println("Modifier: " + Modifier.toString(method.getModifiers()));
+            System.out.println("Method: " + method.toGenericString());
+        }
+        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+
+    }
+
+
+    public List<Method> getPublicSetters(Class<?> clazz){
+
+        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+    }
+
+
+    public List<Field> getFieldsForPublicProperties(Class<?> clazz){
+
+        return Arrays.stream(clazz.getDeclaredFields()).toList();
+
+    }
+
+
+}
