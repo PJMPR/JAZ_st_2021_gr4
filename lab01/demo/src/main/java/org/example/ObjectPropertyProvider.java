e8954b5aa38d1e91ab34391b897ed70c965f4fd3
Michal_K <michal.kalinowski.gd.pl@gmail.com>
2021-10-13 18:33:15 +0200

    2 tests passed!

--- lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -4,38 +4,77 @@ package org.example;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ObjectPropertyProvider {
 
     public List<Method> getPublicGetters(Class<?> clazz) {
+        Method[] methods = clazz.getDeclaredMethods();
 
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
-        Method[] methods = SampleClass.class.getMethods();
-        for (Method method : methods) {
-            System.out.println("Modifier: " + Modifier.toString(method.getModifiers()));
-            System.out.println("Method: " + method.toGenericString());
+        List<Method> passed = new ArrayList<Method>();
+        for(Method method : methods){
+            if(isGetter(method)) {
+                System.out.println("getter: " + method);
+                passed.add(method);
+            }
         }
+        return passed;
     }
-        class SampleClass {
-            private String sampleField;
 
-            public String getSampleField() throws ArrayIndexOutOfBoundsException {
-                return sampleField;
-            }
-
-            public void setSampleField(String sampleField) {
-                this.sampleField = sampleField;
-            }
+    public static boolean isGetter(Method method){
+        if(!method.getName().startsWith("get") && !method.getName().startsWith("is"))
+        {
+            return false;
         }
-
+        if(method.getParameterTypes().length != 0)
+        {
+            return false;
+        }
+        if(void.class.equals(method.getReturnType()))
+        {
+            return false;
+        }
+        if(!Modifier.isPublic(method.getModifiers()))
+        {
+            return false;
+        }
+        return true;
+    }
 
 
 
     public List<Method> getPublicSetters(Class<?> clazz){
+        Method[] methods = clazz.getDeclaredMethods();
+        List<Method> passed = new ArrayList<Method>();
+        for(Method method : methods){
+            if(isSetter(method)) {
+                System.out.println("getter: " + method);
+                passed.add(method);
+            }
+        }
+        return passed;
 
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+    }
+    public static boolean isSetter(Method method){
+        if(!method.getName().startsWith("set"))
+        {
+            return false;
+        }
+        if(method.getParameterTypes().length != 1)
+        {
+            return false;
+        }
+        if(!Modifier.isPublic(method.getModifiers()))
+        {
+            return false;
+        }
+        if(!void.class.equals(method.getReturnType()))
+        {
+            return false;
+        }
+        return true;
     }
 
 
