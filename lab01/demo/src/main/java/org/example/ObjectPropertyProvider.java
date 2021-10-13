2132b4ec34efec88eea422643c03040028d6f5c5
Jakub Skrzyński <s23132@pjwstk.edu.pl>
2021-10-13 18:44:38 +0200

    Exercise 1 and 2 done

--- lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -3,21 +3,50 @@ package org.example;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ObjectPropertyProvider {
 
     public List<Method> getPublicGetters(Class<?> clazz){
+        Method[] methods = clazz.getMethods();
+        ArrayList methodsToReturn = new ArrayList();
 
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
-
+        for(Method method : methods){
+            int modifier = method.getModifiers();
+            if(Modifier.toString(modifier).equals("public")){
+                if(method.getName().startsWith("get") || method.getName().startsWith("is")){
+                    if((method.getParameters().length==0)){
+                        if(!method.getReturnType().getName().equals("void")){
+                            methodsToReturn.add(method);
+                        }
+                    }
+                }
+            }
+        }
+        return methodsToReturn;
     }
 
 
     public List<Method> getPublicSetters(Class<?> clazz){
+        Method[] methods = clazz.getMethods();
+        ArrayList methodsToReturn = new ArrayList();
 
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+        for(Method method : methods){
+            int modifier = method.getModifiers();
+            if(Modifier.toString(modifier).equals("public")){
+                if(method.getName().startsWith("set")){
+                    if((method.getParameters().length==1)){
+                        if(method.getReturnType().getName().equals("void")){
+                            methodsToReturn.add(method);
+                        }
+                    }
+                }
+            }
+        }
+        return methodsToReturn;
     }
 
 
