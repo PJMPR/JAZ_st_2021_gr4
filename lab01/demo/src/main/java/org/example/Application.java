d575df54f0198640e22d2809f48d70ac4c419262
your_username <your_email_address@example.com>
2021-10-13 18:37:31 +0200

    lab 01

--- lab01/demo/src/main/java/org/example/Application.java
+++ lab01/demo/src/main/java/org/example/Application.java
@@ -3,6 +3,7 @@ package org.example;
 public class Application {
 
     public static void main(String[] args){
-
+        ObjectPropertyProvider obj = new ObjectPropertyProvider();
+        obj.getPublicGetters(Subject.class);
     }
 }
--- lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -3,21 +3,48 @@ package org.example;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ObjectPropertyProvider {
 
     public List<Method> getPublicGetters(Class<?> clazz){
-
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
-
+        Method[] methods = clazz.getDeclaredMethods();
+        List<Method> result = new ArrayList<>();
+        for (Method method : methods) {
+            if (method.getName().startsWith("get") || method.getName().startsWith("is")) {
+                if (method.getParameterTypes().length == 0) {
+                    if (!void.class.equals(method.getReturnType())) {
+                        if (Modifier.isPublic(method.getModifiers())) {
+                            System.out.println(method.getName());
+                            result.add(method);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
     }
 
 
     public List<Method> getPublicSetters(Class<?> clazz){
-
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+        Method[] methods = clazz.getDeclaredMethods();
+        List<Method> result = new ArrayList<>();
+        for (Method method : methods) {
+            if (method.getName().startsWith("set")) {
+                if (method.getParameterTypes().length == 1) {
+                    if (void.class.equals(method.getReturnType())) {
+                        if (Modifier.isPublic(method.getModifiers())) {
+                            System.out.println(method.getName());
+                            result.add(method);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
     }
 
 
