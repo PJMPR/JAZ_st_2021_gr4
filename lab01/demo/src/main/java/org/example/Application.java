c2b12333a36a3572fac98bbeff38834eb1d59959
Artur Szulist <arfes716@gmail.com>
2021-10-13 18:18:10 +0200

    v2

--- lab01/demo/src/main/java/org/example/Application.java
+++ lab01/demo/src/main/java/org/example/Application.java
@@ -1,8 +1,30 @@
 package org.example;
 
+import java.lang.reflect.Method;
+
 public class Application {
 
     public static void main(String[] args){
+        ObjectPropertyProvider getsetgo = new ObjectPropertyProvider(); //tworzymy obiekt testujący
 
+        System.out.println("Getters");getsetgo.getPublicGetters(Tester.class);  //wypisuje nam gettery
+        System.out.println("Setters");getsetgo.getPublicSetters(Tester.class);  //bedzie wypisywać settery
+        //System.out.println("Fields");getsetgo.getFieldsForPublicProperties(Tester.class);  //bedzie wypisywać fieldsy
+
+    }
+    public static class Tester{
+        private int id;
+
+        public Tester(int tempId){      ///Konstruktor
+            id = tempId;
+        }
+
+        public int getId(){             ///Getter
+            return id;
+        }
+
+        public void setName(int newId){ ///Setter
+            this.id=newId;
+        }
     }
 }
--- lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -3,29 +3,58 @@ package org.example;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ObjectPropertyProvider {
 
     public List<Method> getPublicGetters(Class<?> clazz){
-        
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+        Method[] methods = clazz.getDeclaredMethods();
+        List listaWynik = new ArrayList();
+        for(Method method : methods) {
+            if(method.getName().startsWith("get")||(method.getName().startsWith("is"))){
+                if(method.getParameterTypes().length==0){
+                    if(Modifier.toString(method.getModifiers()).equals("public")){
+                        if(!void.class.equals(method.getReturnType())){
+                            listaWynik.add(method);
+                        }
+                    }
+                }
+            }
+        }
 
+
+        return listaWynik;
     }
 
 
     public List<Method> getPublicSetters(Class<?> clazz){
-
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+        Method[] methods = clazz.getDeclaredMethods();
+        List listaWynik = new ArrayList();
+        for(Method method : methods){
+            if(method.getName().startsWith("set")){
+                if(method.getParameterTypes().length==1){
+                    if(Modifier.toString(method.getModifiers()).equals("public")){
+                        if(void.class.equals(method.getReturnType())){
+                            listaWynik.add(method);
+                        }
+                    }
+                }
+            }
+        }
+        return listaWynik;
     }
 
 
     public List<Field> getFieldsForPublicProperties(Class<?> clazz){
+        Field[] fields = clazz.getDeclaredFields();
+        Method[] methods = clazz.getDeclaredMethods();
+        
 
         return Arrays.stream(clazz.getDeclaredFields()).toList();
 
     }
-
 
 }
