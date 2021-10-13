ef5f84241564b5e18c6916d5a57ab6d2c493cf17
Jakub Niedźwiedź <s18947@pjwstk.edu.pl>
2021-10-13 18:29:20 +0200

    finish two first methods

--- lab01/demo/src/main/java/org/example/Application.java
+++ lab01/demo/src/main/java/org/example/Application.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import java.lang.reflect.Method;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args){
+        ObjectPropertyProvider Obj = new ObjectPropertyProvider();
+        List<Method> list = Obj.getPublicGetters(Subject.class);
 
+        for(Method m: list){
+            System.out.println(m);
+        }
     }
 }
--- lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -3,21 +3,54 @@ package org.example;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ObjectPropertyProvider {
-
     public List<Method> getPublicGetters(Class<?> clazz){
 
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+        Method[] allMethods = clazz.getDeclaredMethods();
+        List<Method> getters = new ArrayList<>();
+
+
+        for(Method method: allMethods){
+            if(Modifier.isPublic(method.getModifiers())) {
+                if (method.getName().startsWith("get") || method.getName().startsWith("is")) {
+                    if (method.getParameterCount() == 0) {
+                        if (!method.getReturnType().getSimpleName().equals("void")){
+                            getters.add(method);
+                        }
+                    }
+                }
+            }
+        }
+
+        return getters;
 
     }
 
 
     public List<Method> getPublicSetters(Class<?> clazz){
 
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+        Method[] allMethods = clazz.getDeclaredMethods();
+        List<Method> setters = new ArrayList<>();
+
+
+        for(Method method: allMethods){
+            if(Modifier.isPublic(method.getModifiers())) {
+                if (method.getName().startsWith("set")) {
+                    if (method.getParameterCount() == 1) {
+                        if (method.getReturnType().getSimpleName().equals("void")){
+                            setters.add(method);
+                        }
+                    }
+                }
+            }
+        }
+
+        return setters;
     }
 
 
