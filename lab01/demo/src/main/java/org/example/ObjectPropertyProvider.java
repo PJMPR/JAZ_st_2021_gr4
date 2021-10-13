4d6ab90edf8862640d6e0c8ad9f995ae9370e323
Ghostre9000 <77153195+Ghostre9000@users.noreply.github.com>
2021-10-13 18:10:09 +0200

    Update 1
    tests not working

--- lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/demo/src/main/java/org/example/ObjectPropertyProvider.java
@@ -10,15 +10,19 @@ public class ObjectPropertyProvider {
 
     public List<Method> getPublicGetters(Class<?> clazz){
 
-        Method[] methods = SampleClass.class.getMethods();
-        for (Method method : methods) {
-            System.out.println("Modifier: " + Modifier.toString(method.getModifiers()));
-            System.out.println("Method: " + method.toGenericString());
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for(Method method : methods){
+            if(isGetter(method)) System.out.println("getter: " + method);
         }
         return Arrays.stream(clazz.getDeclaredMethods()).toList();
-
     }
-
+    public static boolean isGetter(Method method){
+        if(!method.getName().startsWith("get") && !method.getName().startsWith("is"))      {return false;}
+        if(method.getParameterTypes().length != 0)   {return false;}
+        if(void.class.equals(method.getReturnType())) {return false;}
+        return true;
+        }
 
     public List<Method> getPublicSetters(Class<?> clazz){
 
