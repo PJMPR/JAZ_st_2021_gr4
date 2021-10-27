844e0838ae4a1da5600e481f5cd7a27c9b2d918b
Kinga Marszałkowska <s22206@pjwstk.edu.pl>
2021-10-27 18:30:04 +0200

    Added Annotations.

--- lab02/demo/src/main/java/org/example/validators/Validator.java
+++ lab02/demo/src/main/java/org/example/validators/Validator.java
@@ -1,9 +1,18 @@
 package org.example.validators;
 
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
 public class Validator {
 
     public <TClass> ValidationResult validate(TClass object){
+       Field[] fieldList = object.getClass().getDeclaredFields();
+       for (Field field: fieldList){
+           System.out.println(Arrays.toString(field.getAnnotations()));
+       }
+
 
         return null;
     }
