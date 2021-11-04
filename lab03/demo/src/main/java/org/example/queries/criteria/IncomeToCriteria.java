304e49b3b9357bb19101988ba9b1aa57f4ccbd0d
JSkrzynski <s23132@pjwstk.edu.pl>
2021-11-04 22:22:26 +0100

    all tests passed

---
+++ lab03/demo/src/main/java/org/example/queries/criteria/IncomeToCriteria.java
@@ -0,0 +1,20 @@
+package org.example.queries.criteria;
+
+import org.example.queries.results.Results;
+import org.example.queries.search.SearchParameters;
+import java.util.stream.Collectors;
+
+public class IncomeToCriteria implements Criteria {
+
+    @Override
+    public void meetCriteria(Results results, SearchParameters searchParameters) {
+        if(searchParameters.getIncomeTo() > 0 ){
+            results.setItems(
+                    results.getItems()
+                            .stream().filter(person -> person
+                            .getIncome() <= searchParameters
+                            .getIncomeTo())
+                            .collect(Collectors.toList()));
+        }
+    }
+}
