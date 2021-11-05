0334f5f0804f41f383ebe98145e24b1d31e1be37
Filiposki54 <filip.kierczynski@gmail.com>
2021-11-05 17:24:46 +0100

    lab003

--- lab03/demo/src/main/java/org/example/queries/QueryProcessor.java
+++ lab03/demo/src/main/java/org/example/queries/QueryProcessor.java
@@ -1,16 +1,38 @@
 package org.example.queries;
 
+import org.example.criteria.*;
 import org.example.model.People;
 import org.example.queries.results.Results;
 import org.example.queries.search.SearchParameters;
+
+import java.util.List;
 
 public class QueryProcessor {
 
     public Results GetResults(SearchParameters parameters){
         Results result = new Results();
 
+        List<Criteria> criteria = List.of(
+                new CriteriaFromAge(),
+                new CriteriaGender(),
+                new CriteriaIncomeFrom(),
+                new CriteriaIncomeTo(),
+                new CriteriaName(),
+                new CriteriaSurname(),
+                new CriteriaToAge()
+        );
+
+        criteria.forEach(filter ->
+                filter.meetCriteria(result,parameters));
+
         result.setItems(People.Data);
 
+
+
         return result;
+
+
+
+
     }
 }
