52a10f00f962639b06401ed6ffc54c12397eb95a
NataliaSkonieczna <natalia.skonieczna2001@wp.pl>
2021-11-11 16:20:57 +0100

    should work

--- lab04/demo/src/main/java/org/example/caching/Cache.java
+++ lab04/demo/src/main/java/org/example/caching/Cache.java
@@ -1,26 +1,37 @@
 package org.example.caching;
 
-import org.example.model.Dictionary;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
 
 public class Cache {
+    private static final Cache cache = new Cache();
+    private List<CacheElements> cacheElements = new ArrayList<>();
+
 
     public static Cache getInstance(){
-        return new Cache();
+        return cache;
     }
 
 
     public <T> void add(String key, T item){
-
+        CacheElements cache = new CacheElements();
+        cache.key = key;
+        cache.item = item;
+        cacheElements.add(cache);
     }
 
     public <T> T get(String key, Class<T> clazz){
 
-        return (T) clazz.cast(new Object());
+        return (T) cacheElements.stream()
+                .filter(item->key.equals(item.key)
+                        &&clazz.equals(item.item.getClass()))
+                .findAny().get().item;
     }
 
     public Object get(String key){
-        return null;
+        return cacheElements.stream()
+                .filter(cache -> key.equals(cache.key))
+                .findAny().get().item;
     }
 }
---
+++ lab04/demo/src/main/java/org/example/caching/CacheElements.java
@@ -0,0 +1,6 @@
+package org.example.caching;
+
+public class CacheElements {
+    public Object item;
+    public String key;
+}
