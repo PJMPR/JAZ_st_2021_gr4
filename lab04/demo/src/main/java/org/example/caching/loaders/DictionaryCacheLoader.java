d223f0b8545904ebdc541ed6f276dca60baf0fa5
arktisk_ulv <s23435@pjwstk.edu.pl>
2021-11-12 19:06:15 +0100

    all tests pass

--- lab04/demo/src/main/java/org/example/caching/loaders/DictionaryCacheLoader.java
+++ lab04/demo/src/main/java/org/example/caching/loaders/DictionaryCacheLoader.java
@@ -1,6 +1,20 @@
 package org.example.caching.loaders;
 
-public class DictionaryCacheLoader {
+import org.example.caching.Cache;
+import org.example.caching.DictionaryFilesProvider;
+import org.example.model.Dictionary;
+import java.util.List;
 
-    public void load(){}
+public class DictionaryCacheLoader implements CacheLoader{
+
+    public final Cache cache = Cache.getInstance();
+
+    public void load(){
+
+        DictionaryFilesProvider provider = new DictionaryFilesProvider();
+        List<Dictionary> records = provider.database();
+        String file = provider.file();
+        records.forEach(record -> cache.add(file, record));
+
+    }
 }
