d3602495595062a5de809fe3081efb154dcc5d67
Ghost <ghostre1@wp.pl>
2021-11-28 14:50:29 +0100

    All tests passed

---
+++ lab06/demo/src/main/java/com/example/demo/controller/Calculator.java
@@ -0,0 +1,53 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+
+@Component
+public class Calculator {
+    public ArrayList<Installment> calculateInstalments(Timetable timetable) {
+        ArrayList<Installment> installments = new ArrayList<>();
+        double capital = 0;
+
+        if (timetable.getInstallmentType() == InstallmentType.CONSTANT) {
+
+            ConstantCalculations(timetable, installments, capital);
+
+        } else if (timetable.getInstallmentType() == InstallmentType.DECREASING) {
+
+            DecreasingCalculations(timetable, installments, capital);
+
+        }
+
+        return installments;
+    }
+
+    private void DecreasingCalculations(Timetable timetable, ArrayList<Installment> installments, double capital) {
+        double capitalToPay = timetable.getAmount();
+        double percentage = timetable.getPercentage();
+        long count = timetable.getInstallmentCount();
+        double amount;
+        double installment = timetable.getAmount() * percentage * Math.pow((percentage + 1), count) / (Math.pow((percentage + 1), count) - 1);
+
+        for (int i = 0; i < timetable.getInstallmentCount(); i++) {
+            double interest = capitalToPay * percentage;
+            amount = installment + interest;
+            installments.add(new Installment(timetable.getId(), i + 1, capital, interest, timetable.getFixedFee(), capitalToPay, amount));
+            capital += amount;
+            capitalToPay -= amount;
+        }
+    }
+
+    private void ConstantCalculations(Timetable timetable, ArrayList<Installment> installments, double capital) {
+        double totalInterest = (timetable.getPercentage() * timetable.getAmount()) + (timetable.getFixedFee() * timetable.getInstallmentCount());
+        double monthlyInterest = totalInterest / timetable.getInstallmentCount();
+        double capitalToPay = totalInterest + timetable.getAmount();
+        double installment = capitalToPay / timetable.getInstallmentCount();
+
+        for (int i = 0; i < timetable.getInstallmentCount(); i++) {
+            installments.add(new Installment(timetable.getId(), i + 1, capital, monthlyInterest, timetable.getFixedFee(), capitalToPay, installment));
+            capital += installment;
+            capitalToPay -= installment;
+        }
+    }
+}
