b3e52aec4c6f2782dd1cd2ea3f391c11a129c700
Filiposki54 <filip.kierczynski@gmail.com>
2021-12-14 10:31:56 +0100

    lab 8

--- lab08/demo/demo/src/main/java/com/example/demo/contract/MovieDto.java
+++ lab08/demo/demo/src/main/java/com/example/demo/contract/MovieDto.java
@@ -1,55 +1,43 @@
 package com.example.demo.contract;
 
+import com.example.demo.model.Actor;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Entity;
 import java.util.List;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class MovieDto {
-    int id;
-    @JsonProperty("imdb_id")
-    String imdbId;
-    @JsonProperty("original_title")
-    String originalTitle;
-    String overview;
-    List<GenreDto> genres;
 
-    public int getId() {
-        return id;
-    }
+    private String title;
+    private int id;
+    @JsonProperty("runtime")
+    private int runtime;
+    @JsonProperty("overview")
+    private String overview;
+    private int year;
+    private String rating;
 
-    public void setId(int id) {
+    private List<Actor> actors;
+
+    private String imdb_id;
+
+    public MovieDto(String title, int id, int runtime, String overview, int year, String rating, List<Actor> actors, String imdb_id) {
+        this.title = title;
         this.id = id;
-    }
-
-    public String getImdbId() {
-        return imdbId;
-    }
-
-    public void setImdbId(String imdbId) {
-        this.imdbId = imdbId;
-    }
-
-    public String getOriginalTitle() {
-        return originalTitle;
-    }
-
-    public void setOriginalTitle(String originalTitle) {
-        this.originalTitle = originalTitle;
-    }
-
-    public String getOverview() {
-        return overview;
-    }
-
-    public void setOverview(String overview) {
+        this.runtime = runtime;
         this.overview = overview;
+        this.year = year;
+        this.rating = rating;
+        this.actors = actors;
+        this.imdb_id = imdb_id;
     }
 
-    public List<GenreDto> getGenres() {
-        return genres;
-    }
 
-    public void setGenres(List<GenreDto> genres) {
-        this.genres = genres;
-    }
 }
---
+++ lab08/demo/demo/src/main/java/com/example/demo/controllers/MoviesController.java
@@ -0,0 +1,62 @@
+package com.example.demo.controllers;
+
+import com.example.demo.contract.IMDBMovieDto;
+import com.example.demo.contract.MovieDto;
+import com.example.demo.service.FilmService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Calendar;
+
+
+public class MoviesController {
+
+
+    FilmService filmService;
+
+    public MoviesController(FilmService filmService){
+        this.filmService = filmService;
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public ResponseEntity<MovieDto> getData(@PathVariable("id") int id){
+        return ResponseEntity.ok(filmService.getMoveInfo(id));
+    }
+
+
+
+    @GetMapping
+    @RequestMapping("/imdb/{id}")
+    public ResponseEntity<IMDBMovieDto> getDataIMDB(@PathVariable("id") String id){
+        return ResponseEntity.ok(filmService.getMovieInfoFromIMDB(id));
+    }
+
+
+
+    @PostMapping("/updater/status")
+    public ResponseEntity getSystemStatus(){
+        return ResponseEntity.ok(filmService.getSystemStatusInfo());
+    }
+
+
+    @GetMapping
+    @RequestMapping("/updater/reload")
+    public ResponseEntity reloadData() {
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        return ResponseEntity.ok(filmService.reloadData());
+    }
+
+    @GetMapping
+    @RequestMapping("/updater/reload/{year}")
+    public ResponseEntity reloadDataByYear(@PathVariable int year) {
+        return ResponseEntity.ok(filmService.reloadDataByYear(year));
+    }
+}
