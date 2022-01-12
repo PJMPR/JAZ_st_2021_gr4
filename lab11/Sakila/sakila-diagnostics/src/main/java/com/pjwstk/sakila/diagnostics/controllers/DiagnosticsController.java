956855160f7dc9f10699a3ffb2d786111864a2d8
adam urbanowicz <adam.urbanow@gmail.com>
2022-01-12 17:46:25 +0100

    lab10 lab11

---
+++ lab11/Sakila/sakila-diagnostics/src/main/java/com/pjwstk/sakila/diagnostics/controllers/DiagnosticsController.java
@@ -0,0 +1,21 @@
+package com.pjwstk.sakila.diagnostics.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("diagnostics")
+public class DiagnosticsController {
+
+    @GetMapping("status")
+    public ResponseEntity status(){
+        return ResponseEntity.ok("ALIVE");
+    }
+
+    @GetMapping("selftest")
+    public ResponseEntity selftest(){
+        return ResponseEntity.ok("Selftest");
+    }
+}
